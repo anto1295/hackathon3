54a74f63b7e6ead9671cd16e851b58857bce945e
Anthony Salvatierra <anto1295salva@gmail.com>
2022-10-21 10:32:30 +0200

    correct migration

--- database/migrations/2022_10_21_080455_create_owners_table.php
+++ database/migrations/2022_10_21_080455_create_owners_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->id();
-            $table->string('first name')->nullable();
+            $table->string('first_name')->nullable();
             $table->string('surname')->nullable();
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
